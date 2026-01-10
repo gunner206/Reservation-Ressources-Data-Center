a9d86d682ff7288c8058881d41cc773d6ada3ff9
zakichaimae-byte <zaki.chaimae@etu.uae.ac.ma>
2026-01-10 23:59:20 +0100

    Fix: Correction nom vue login et ajout formulaire html

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+// Page d'accueil (Publique)
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+// Routes pour les invités (Non connectés)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+// Routes protégées (Connectés seulement)
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    // Page Dashboard temporaire pour tester
+    Route::get('/dashboard', function () {
+        return "Bienvenue " . auth()->user()->name . " ! Votre rôle est : " . auth()->user()->role;
+    });
 });
