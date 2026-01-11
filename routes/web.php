aedf61ff2dfa23c64363425595ea6b129c1dccc4
houssam-icon <hossam9999phone@gmail.com>
2026-01-11 01:32:32 +0100

    new layout

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RessourceController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
-
-// TESTS (à supprimer plus tard)
-Route::get('/test-simple', function() {
-    return "TEST SIMPLE - OK";
-});
-
-// ROUTES RESSOURCES (version manuelle)
-Route::get('/ressources', [RessourceController::class, 'index'])->name('ressources.index');
-Route::get('/ressources/create', [RessourceController::class, 'create'])->name('ressources.create');
-Route::post('/ressources', [RessourceController::class, 'store'])->name('ressources.store');
-
-// Test ultra-simple
-Route::get('/test-ressources', function() {
-    return "TEST RESSOURCES - DIRECT";
-});
-// Routes manquantes pour CRUD complet
-Route::get('/ressources/{id}', [RessourceController::class, 'show'])->name('ressources.show');
-Route::get('/ressources/{id}/edit', [RessourceController::class, 'edit'])->name('ressources.edit');
-Route::put('/ressources/{id}', [RessourceController::class, 'update'])->name('ressources.update');
-Route::delete('/ressources/{id}', [RessourceController::class, 'destroy'])->name('ressources.destroy');
-Route::get('/reservation/create', [ReservationController::class, 'create']);
-Route::post('/reservation', [ReservationController::class, 'store']);
